2aee8d8a1c6f3c5f60aa759113c84296b4e0c0de
Dmitry Kalenyuk <dmitrykalenyuk@gmail.com>
2017-12-05 14:36:45 +0200

    + .config.php was adaptated to woek on remote server

--- templates/main.php
+++ templates/main.php
@@ -1,21 +1,7 @@
-<?php require_once __DIR__ . '/_header.php';
-use App\App;
-use App\Models\Post;
-?>
-
-
-<div>
 <?php
-//TODO Make a cycle that will increase ID by 1
-
-$post = Post::find(1);
-echo '<h3>'.$post->title.'</h3><br>'.
-    $post->content.'<br>'.
-    '<h4>'.$post->author.'</h4>';
-
-?>
-</div>
-
-
-
-<?php //require_once __DIR__ . '/_footer.php'; ?>
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 05.12.2017
+ * Time: 14:06
+ */
