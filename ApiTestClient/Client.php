35dc5c069cf44f22554a5d27bc49f80f56ab7efe
Dmitry Kalenyuk <dmitrykalenyuk@gmail.com>
2017-12-08 19:23:27 +0200

    + createpost code to fix

--- ApiTestClient/Client.php
+++ ApiTestClient/Client.php
@@ -27,4 +27,20 @@ class Client
         return $response;
     }
 
+    //TODO: make it work!!!
+    public static function createPost()
+    {
+
+        $newpost = ['title' => 'titleCURL', 'content' => 'content', 'author' => 'author'];
+        $url = curl_init("http://192.168.1.199/dmitry.kalenyuk/rest-api-codeit/public/posts/create");
+        curl_setopt($url, CURLOPT_USERPWD, "root:root");
+        curl_setopt($url, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($url, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($url, CURLOPT_POSTFIELDS, $newpost);
+        $response = curl_exec($url);
+        curl_close($url);
+        echo $response;
+        return $response;
+    }
+
 }
--- ApiTestClient/index.php
+++ ApiTestClient/index.php
@@ -3,3 +3,8 @@ require_once __DIR__.'/Client.php';
 use Client\Client as Client;
 //Client::fetchAll();
 //Client::fetchID(1);
+?>
+
+<form action="<?php Client::fetchAll(); ?>">
+    <input type="submit"><button type="submit" formaction="<?php Client::fetchAll(); ?>">Fetch All</button>
+</form>
