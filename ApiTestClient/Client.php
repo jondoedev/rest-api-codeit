ebcf1c35b9bcf0342a1cdcf928ab15315dac3bc7
Dmitry Kalenyuk <dmitrykalenyuk@gmail.com>
2017-12-11 17:16:24 +0200

    fully working test client added

--- ApiTestClient/Client.php
+++ ApiTestClient/Client.php
@@ -2,7 +2,9 @@
 namespace Client;
 class Client
 {
-
+    /**
+     * get all existing posts
+     */
     public static function fetchAll()
     {
         $url = curl_init("http://192.168.1.199/dmitry.kalenyuk/rest-api-codeit/public/posts");
@@ -15,6 +17,12 @@ class Client
         return $response;
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     *
+     * get single post by id
+     */
     public static function fetchID($id)
     {
         $url = $url = curl_init("http://192.168.1.199/dmitry.kalenyuk/rest-api-codeit/public/posts/".$id);
@@ -28,18 +36,59 @@ class Client
     }
 
     //TODO: make it work!!!
-    public static function createPost()
+    public static function createPost($request)
     {
-
-        $newpost = ['title' => 'titleCURL', 'content' => 'content', 'author' => 'author'];
+        $data = json_encode($request);
+//        $newpost = ['title' => $title, 'content' => $content, 'author' => $author];
         $url = curl_init("http://192.168.1.199/dmitry.kalenyuk/rest-api-codeit/public/posts/create");
         curl_setopt($url, CURLOPT_USERPWD, "root:root");
         curl_setopt($url, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($url, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($url, CURLOPT_POSTFIELDS, $newpost);
+//        curl_setopt($url, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($url, CURLOPT_POST, 1);
+        curl_setopt($url, CURLOPT_POSTFIELDS, $data);
+        $response = curl_exec($url);
+        curl_close($url);
+
+        echo $response;
+        return $response;
+    }
+
+    public static function deletePost($id)
+    {
+        $url = curl_init("http://192.168.1.199/dmitry.kalenyuk/rest-api-codeit/public/posts/delete/".$id);
+        curl_setopt($url, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($url, CURLOPT_CUSTOMREQUEST, "DELETE");
+        curl_setopt($url, CURLOPT_USERPWD, "root:root");
         $response = curl_exec($url);
         curl_close($url);
         echo $response;
+        return $response;
+    }
+
+    public static function editPost($request)
+    {
+        $data = json_encode($request);
+        $url = curl_init("http://192.168.1.199/dmitry.kalenyuk/rest-api-codeit/public/posts/edit/54");
+
+        ob_start();
+//        echo "<pre>";
+//        $out = fopen('php://output', 'w');
+
+        curl_setopt($url, CURLOPT_USERPWD,"root:root");
+        curl_setopt($url, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($url, CURLOPT_HTTPHEADER, array('Content-Type: application/json',));
+        curl_setopt($url, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_setopt($url, CURLOPT_POSTFIELDS, $data);
+//        curl_setopt($url, CURLOPT_VERBOSE, true);
+//        curl_setopt($url, CURLOPT_STDERR, $out);
+
+        $response = curl_exec($url);
+
+//        fclose($out);
+
+        curl_close($url);
+        echo $response;
+//        echo "</pre>";
         return $response;
     }
 
--- ApiTestClient/index.php
+++ ApiTestClient/index.php
@@ -1,10 +1,11 @@
 <?php
 require_once __DIR__.'/Client.php';
 use Client\Client as Client;
-//Client::fetchAll();
-//Client::fetchID(1);
-?>
 
-<form action="<?php Client::fetchAll(); ?>">
-    <input type="submit"><button type="submit" formaction="<?php Client::fetchAll(); ?>">Fetch All</button>
-</form>
+$data = [
+    'title' => 'TEST',
+    'content' => 'TESTTEST',
+    'author' => 'TESTTEST'];
+//Client::createPost($data);
+//Client::createPost($data);
+Client::fetchAll();
