126567b68b010454b60fc6b4c602918390f536ad
Dmitry Kalenyuk <dmitrykalenyuk@gmail.com>
2018-02-19 17:24:34 +0200

    + generate 'unix_created_at' property automatically on post create action

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -5,5 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model {
+
     protected $guarded = array();
+    public function setCreatedAtAttribute()
+    {
+        $this->attributes['unix_created_at'] = ((new \DateTime())->getTimestamp());
+
+    }
 }
